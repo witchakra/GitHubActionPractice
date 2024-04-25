3be36e954c68b60ca8cec18b516a2d03ff917091
witchakra <131113499+witchakra@users.noreply.github.com>
2024-04-25 19:41:35 +0700

    clone file from techer

--- config.js
+++
@@ -1,3 +0,0 @@
-const crypto = require("crypto");
-const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
